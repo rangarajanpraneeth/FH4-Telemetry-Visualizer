b152fc2f2b5bf0284da6877c30223f1e8452d5b7
srithwak <srithwak@gmail.com>
2024-01-19 16:15:47 -0500

    t

--- src/scripts/graph.py
+++ src/scripts/graph.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # only works in the same directory
 def get_csv_data_local(filename):
@@ -9,22 +10,20 @@ def get_csv_data_local(filename):
 def extract_csv(data):
     return {column: data[column].to_numpy() for column in data.columns[data.notna().any()].tolist()}
 
-def plot_space_cVal(fileName, val, vtype):
+def plot_space_cVal(fileName, val, type):
     data = extract_csv(get_csv_data_local(fileName))
     cVal = data[val]
     plt.style.use('dark_background')
-    if vtype == 'space':
+    if type == 'space':
         plt.axis('equal')
         plt.scatter(data['carPositionX'], data['carPositionZ'], c=cVal, cmap='RdYlGn_r', marker='o', label='Data')
         plt.xlabel('X-Direction')
         plt.ylabel('Y-Direction')
-    elif vtype == 'time':
+    elif type == 'time':
         timestampArr = data['timestamp']
         initialTime = timestampArr[0]
         for i in range(len(timestampArr)):
             timestampArr[i] = timestampArr[i] - initialTime
-        # plt.style.use('dark_background')
-        # plt.axis('equal')
         plt.scatter(timestampArr,cVal ,c=cVal, cmap='RdYlGn_r', marker='o', label='Data')
         plt.xlabel('Time')
         plt.ylabel(val)
@@ -35,4 +34,30 @@ def plot_space_cVal(fileName, val, vtype):
     plt.title(val)
     plt.show()
 
-plot_space_cVal('speed.csv', 'carSpeed', 'time')
+def plot_3d_graph(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+        # ax.set_aspect('equal')
+        # ax.set_ylim(-1000,1000)
+    # Scatter plot
+    ax.scatter(x, y, z, c='r', marker='o')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    # ax.set_aspect('equal')
+    ax.axis('equal')
+    # ax.set_box_aspect([1, 1, 1], adjustable='box')
+
+    plt.show()
+
+plot_space_cVal('creampie.csv', 'carPositionY', 'space')
+
+# Example data
+# x_data = [1, 2, 3, 4, 5]
+# y_data = [2, 3, 4, 5, 6]
+# z_data = [5, 6, 7, 8, 9]
+
+# Plot the 3D graph
+data = extract_csv(get_csv_data_local('creampie.csv'))
+plot_3d_graph(data['carPositionX'], data['carPositionY'], data['carPositionZ'])

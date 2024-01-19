a83654d16034b52b8fe8b0453933a04e83a6c392
srithwak <srithwak@gmail.com>
2024-01-19 13:33:27 -0500

    test

--- src/scripts/graph.py
+++ src/scripts/graph.py
@@ -9,33 +9,29 @@ def get_csv_data_local(filename):
 def extract_csv(data):
     return {column: data[column].to_numpy() for column in data.columns[data.notna().any()].tolist()}
 
-def plot_space_cVal(fileName, val):
+def plot_space_cVal(fileName, val, type):
     data = extract_csv(get_csv_data_local(fileName))
     cVal = data[val]
     plt.style.use('dark_background')
-    plt.axis('equal')
-    plt.scatter(data['carPositionX'], data['carPositionY'], c=cVal, cmap='RdYlGn_r', marker='o', label='Data')
+    if type == 'space':
+        plt.axis('equal')
+        plt.scatter(data['carPositionX'], data['carPositionY'], c=cVal, cmap='RdYlGn_r', marker='o', label='Data')
+        plt.xlabel('X-Direction')
+        plt.ylabel('Y-Direction')
+    elif type == 'time':
+        timestampArr = data['timestamp']
+        initialTime = timestampArr[0]
+        for i in range(len(timestampArr)):
+            timestampArr[i] = timestampArr[i] - initialTime
+        plt.style.use('dark_background')
+        plt.scatter(timestampArr,cVal ,c=cVal, cmap='RdYlGn_r', marker='o', label='Data')
+        plt.xlabel('Time')
+        plt.ylabel(val)
+    else:
+        print('Wrong Parameter passed')
     cbar = plt.colorbar()
     cbar.set_label(val)
-    plt.xlabel('X Values')
-    plt.ylabel('Y Values')
     plt.title(val)
     plt.show()
 
-def plot_time_cVal(fileName, val):
-    data = extract_csv(get_csv_data_local(fileName))
-    cVal = data[val]
-    timestampArr = data['timestamp']
-    initialTime = timestampArr[0]
-    for i in range(len(timestampArr)):
-        timestampArr[i] = timestampArr[i] - initialTime
-    plt.style.use('dark_background')
-    plt.scatter(timestampArr,cVal ,c=cVal, cmap='RdYlGn_r', marker='o', label='Data')
-    cbar = plt.colorbar()
-    cbar.set_label(val)
-    plt.xlabel('Time')
-    plt.ylabel(val)
-    plt.title(val)
-    plt.show()
-
-plot_time_cVal('creampie.csv', 'carAccelerationX')
+plot_space_cVal('creampie.csv', 'carAccelerationX', 'space')

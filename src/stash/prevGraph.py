a42ef9e2c716cb7514db1950fd5a00c860af8dc3
rangarajanpraneeth <rangarajanpraneeth@gmail.com>
2024-01-19 22:39:15 -0500

    graph

--- src/stash/prevGraph.py
+++ src/stash/prevGraph.py
@@ -22,3 +22,40 @@ def plot_cVal(fileName, val):
     plt.show()
 
 plot_cVal('creampie.csv', 'carSpeed')
+
+
+
+
+
+
+
+
+
+# import os
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+
+# def graph3D(filePath, colorProperty):
+#     dataFrame = pd.read_csv(
+#         os.path.abspath(os.path.join(os.path.dirname(__file__), "../data", filePath))
+#     )
+#     plt.style.use("dark_background")
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection="3d")
+#     ax.xaxis.set_pane_color((0, 0, 0))
+#     ax.yaxis.set_pane_color((0, 0, 0))
+#     ax.zaxis.set_pane_color((0, 0, 0))
+#     ax.scatter(
+#         dataFrame["carPositionX"],
+#         dataFrame["carPositionZ"],
+#         dataFrame["carPositionY"],
+#         # c="r",
+#         c=dataFrame[colorProperty],
+#         cmap="RdYlGn_r",
+#     )
+#     ax.axis("equal")
+#     plt.show()
+
+
+# graph3D("recording-1.csv", "carSpeed")

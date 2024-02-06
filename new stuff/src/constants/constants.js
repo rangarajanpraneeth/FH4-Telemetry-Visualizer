37c77cef3e2ea49fd5a3b8d53fc4367fc84d56ae
rangarajanpraneeth <rangarajanpraneeth@gmail.com>
2024-02-06 16:59:18 -0500

    heheh

---
+++ new stuff/src/constants/constants.js
@@ -0,0 +1,20 @@
+const classes = {
+   0: 'D',
+   1: 'C',
+   2: 'B',
+   3: 'A',
+   4: 'S1',
+   5: 'S2',
+   6: 'X',
+}
+
+const drivetrains = {
+   0: 'FWD',
+   1: 'RWD',
+   2: 'AWD'
+}
+
+module.exports = {
+   classes,
+   drivetrains
+}
